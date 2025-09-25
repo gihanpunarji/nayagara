90c16875049cfaef26d53ceaab1362b911736367
Gihan <gihanpunarji@gmail.com>
2025-09-25 15:55:06 +0530

    fixed

--- server/src/modules/shared/routes/commonRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { mobile, verifyOtp } = require('../utils/mobileVerify');
-
-const router = express.Router();
-
-router.post('/send-otp', mobile);
-router.post('/verify-otp', verifyOtp);
-
-module.exports = router;
